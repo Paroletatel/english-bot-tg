1aabdb3251712dd3a6b03c97dc3eedd595d07db6
Regina Shakirova <reginashakirova@Reginas-MacBook-Pro.local>
2024-08-20 17:12:28 +0300

    fix json gpt

--- index.js
+++ index.js
@@ -347,8 +347,9 @@ const start = () => {
         const res = await openAi.chat.completions.create({
             model: 'gpt-4o',
             messages: [
-                {role: "assistant" || "user", content: prompt}
+                {role: "user", content: prompt}
             ],
+            response_format: { type: 'json_object' },
             temperature: 0.7
         });
         
