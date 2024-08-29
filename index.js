8a6e27dca65ba592427213476239292e3d179389
Regina Shakirova <reginashakirova@Reginas-MacBook-Pro.local>
2024-08-29 17:33:33 +0300

    fix gpt

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const OpenAI  = require('openai');
 const HttpsProxyAgent = require('https-proxy-agent')
 const dedent = require('dedent')
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const httpsAgent = new HttpsProxyAgent({
     host: '45.141.185.132',
